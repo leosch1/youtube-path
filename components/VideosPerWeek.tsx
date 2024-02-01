4a5963cfe3580f9840357d936efca32747bd0548
leosch1 <leoschoechli@bluewin.ch>
2024-02-01 17:28:44 +0700

    add VideosPerWeek

---
+++ components/VideosPerWeek.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
+
+interface DataPoint {
+  date: Date;
+  value: number;
+}
+
+interface VideosPerWeekProps {
+  data: DataPoint[];
+}
+
+const VideosPerWeek: React.FC<VideosPerWeekProps> = ({ data }) => {
+  const d3Container = useRef<SVGSVGElement | null>(null);
+
+  useEffect(() => {
+    if (data && d3Container.current) {
+      const svg = d3.select(d3Container.current);
+
+      // Clear the SVG before drawing new content
+      svg.selectAll("*").remove();
+
+      const tickColor = getComputedStyle(document.documentElement).getPropertyValue('--secondary-text-color').trim();
+
+      const totalWidth = d3Container.current.clientWidth;
+      const totalHeight = d3Container.current.clientHeight;
+      const margin = { top: 20, right: 10, bottom: 5, left: 70 };
+
+      // Create the x scale
+      const x = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.value) ?? 0])
+        .range([margin.left, totalWidth - margin.right]);
+
+      // Create the y scale
+      const y = d3.scaleTime()
+        .domain(d3.extent(data, d => d.date) as [Date, Date])
+        .range([margin.top, totalHeight - margin.bottom]);
+
+      // Define the step function for the stepped line
+      const line = d3.line<DataPoint>()
+        .x(d => x(d.value))
+        .y(d => y(d.date))
+        .curve(d3.curveStepAfter); // Use the step after curve to create the stepped look
+
+      // Add the path using the line generator
+      svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-width", 2)
+        .attr("d", line);
+
+      // Add the X Axis
+      const xAxis = svg.append("g")
+        .attr("transform", `translate(0,${margin.top})`)
+        .call(d3.axisTop(x));
+
+      xAxis.selectAll("text")
+        .attr("fill", tickColor);
+
+      // Add vertical gridlines
+      xAxis.selectAll("line")
+        .clone()
+        .attr("y2", totalHeight - margin.top - margin.bottom)
+        .attr("stroke-opacity", 0.1);
+
+      // Add the Y Axis
+      const yAxis = svg.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y).tickFormat((domainValue) => d3.timeFormat("%d %b %Y")(domainValue as Date)));
+
+      // Change the color of the tick labels
+      yAxis.selectAll("text")
+        .attr("fill", tickColor);
+    }
+  }, [data]);
+
+  return (
+    <svg
+      className="d3-component"
+      width="100%"
+      height="1000"
+      ref={d3Container}
+      preserveAspectRatio="xMidYMid meet"
+    />
+  );
+};
+
+export default VideosPerWeek;
