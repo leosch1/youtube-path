1d132de17825de7865aca1db9bfa22650744dcd1
leosch1 <leoschoechli@bluewin.ch>
2024-02-01 20:39:23 +0700

    add TotalVideoCount

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,26 @@
 
 import React, { useRef, useState } from 'react';
 import styles from "./page.module.css";
-import { getVideosPerWeek } from './utils'; // adjust the path as needed
+import { getVideosPerWeek } from '../utils/utils';
 import LandingZone from '../components/LandingZone';
 import VideosPerWeek from "../components/VideosPerWeek";
+import TotalVideoCount from '../components/TotalVideoCount';
 import { VideoData } from "../types/types";
 
 export default function Home() {
   const watchHistoryDataRef = useRef(null);
-  const [videosPerWeek, setVideosPerWeek] = useState<VideoData[]>([]);
+  const [videosPerWeekData, setVideosPerWeekData] = useState<VideoData[]>([]);
 
   const handleDataChange = (data: any) => {
     watchHistoryDataRef.current = data;
     const result = getVideosPerWeek(data);
-    setVideosPerWeek(result);
+    setVideosPerWeekData(result);
+  };
+
+  const totalVideoCountData = {
+    startDate: new Date('2022-07-06'),
+    endDate: new Date('2023-05-18'),
+    videoCount: 12941
   };
 
   return (
@@ -24,10 +31,11 @@ export default function Home() {
         <div className={styles.sideDiagram}>
           <div className={styles.videosPerWeek}>
             <h2>Videos per week</h2>
-            <VideosPerWeek data={videosPerWeek} />
+            <VideosPerWeek data={videosPerWeekData} />
           </div>
         </div>
         <div className={styles.mainDiagrams}>
+          <TotalVideoCount data={totalVideoCountData} />
         </div>
       </div>
     </main>
