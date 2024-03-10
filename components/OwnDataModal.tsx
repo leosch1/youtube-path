5087cc81732d492807810bdf6c0a90c6931cd09e
leosch1 <35605161+leosch1@users.noreply.github.com>
2024-03-10 09:50:00 +0100

    add step indicator

--- components/OwnDataModal.tsx
+++ components/OwnDataModal.tsx
@@ -10,7 +10,20 @@ const OwnDataModal: FC<OwnDataModalProps> = ({ onClose, fileInputRef }) => {
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modalContent}>
-                <h2>OwnDataModal content here</h2>
+                <div className={styles.head}>
+                    <h2>How to get your watch history using Google Takeout?</h2>
+                    <div className={styles.stepsContainer}>
+                        <div className={styles.stepLine}></div> {/* This is the line connecting the circles */}
+                        <div className={styles.stepIndicator}>
+                            <div className={`${styles.step} ${styles.stepActive}`}>1</div>                            <div className={styles.step}>2</div>
+                            <div className={styles.step}>3</div>
+                            <div className={styles.step}>4</div>
+                            <div className={styles.step}>5</div>
+                            <div className={styles.step}>6</div>
+                        </div>
+                    </div>
+                </div>
+                
                 <button onClick={() => fileInputRef.current?.click()}>Select File</button>
                 <button onClick={onClose}>Close</button>
             </div>
