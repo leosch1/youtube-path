d0cadf08bfaa04414ce2b4c52452a5d7abcc0a2d
leosch1 <35605161+leosch1@users.noreply.github.com>
2024-03-11 21:55:34 +0100

    start adding modal content

--- components/OwnDataModal.tsx
+++ components/OwnDataModal.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import styles from './OwnDataModal.module.css';
+import Image from 'next/image'
 
 interface OwnDataModalProps {
     onClose: () => void;
@@ -9,7 +10,7 @@ interface OwnDataModalProps {
 const OwnDataModal: FC<OwnDataModalProps> = ({ onClose, fileInputRef }) => {
     return (
         <div className={styles.modalOverlay}>
-            <div className={styles.modalContent}>
+            <div className={styles.container}>
                 <div className={styles.head}>
                     <h2>How to get your watch history using Google Takeout?</h2>
                     <div className={styles.stepsContainer}>
@@ -23,7 +24,24 @@ const OwnDataModal: FC<OwnDataModalProps> = ({ onClose, fileInputRef }) => {
                         </div>
                     </div>
                 </div>
-                
+
+                <div className={styles.content}>
+                    <div>
+                        <p>In order to analyse your personal watch history, you can download it from Google Takeout.</p>
+                        <p>Takeout is a programme by Google which allows users of Google services (e.g. YouTube) to get their personal data.</p>
+                        <p>Once you have downloaded your watch history, you can use this webpage to analyse it.</p>
+                        <p>We take data privacy seriously. That is why your data will never leave your device and is not accessible by us at any point.</p>
+                    </div>
+
+                    <div>
+                        {/* <Image
+                            src="/images/google-takeout-1.png"
+                            layout="fill"
+                            objectFit="contain"
+                            alt="Picture of the author"
+                        /> */}
+                    </div>
+                </div>
                 <button onClick={() => fileInputRef.current?.click()}>Select File</button>
                 <button onClick={onClose}>Close</button>
             </div>
