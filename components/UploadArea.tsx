519fc3677c58f73515659b16e8a46445221bface
leosch1 <leoschoechli@bluewin.ch>
2024-03-19 21:58:41 +0100

    move upload area to separte component

---
+++ components/UploadArea.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './UploadArea.module.css';
+
+const UploadArea: FC = () => {
+    return (
+        <div className={styles.uploadArea}>
+            {/* Add any additional content or functionality here */}
+        </div>
+    );
+};
+
+export default UploadArea;
