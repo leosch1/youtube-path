290b0170d2cd88de4ae957768f3f7f372c6e9992
leosch1 <35605161+leosch1@users.noreply.github.com>
2024-03-24 13:28:58 +0100

    close modal on progress done

--- components/LandingZone.tsx
+++ components/LandingZone.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useRef, ChangeEvent, useState } from 'react';
+import React, { FC, useRef, ChangeEvent, useState, useEffect, useContext } from 'react';
 import Image from 'next/image';
 import styles from './LandingZone.module.css';
-import { WatchHistoryEntry } from "../types/types";
+import { WatchHistoryEntry } from '../types/types';
 import OwnDataModal from './OwnDataModal';
+import { ProgressContext } from '../contexts/ProgressContext';
 
 interface LandingZoneProps {
   setData: (data: WatchHistoryEntry[]) => void;
@@ -12,7 +13,18 @@ const LandingZone: FC<LandingZoneProps> = ({ setData }) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [isModalOpen, setModalOpen] = useState(false);
 
-  const handleExploreClick = () => {
+  const progress = useContext(ProgressContext);
+
+  useEffect(() => {
+    if (progress === 1) {
+      setTimeout(() => { // Wait for 1 second before closing modal
+        setModalOpen(false);
+        goToStartingComponent();
+      }, 1000);
+    }
+  }, [progress]);
+
+  const goToStartingComponent = () => {
     const element = document.getElementById('starting-component');
     element?.scrollIntoView({ behavior: 'smooth' });
   };
@@ -42,7 +54,7 @@ const LandingZone: FC<LandingZoneProps> = ({ setData }) => {
         />
       </div>
       <div className={styles.buttons}>
-        <button className={`${styles.button} ${styles.exploreButton}`} onClick={handleExploreClick}>Explore an example</button>
+        <button className={`${styles.button} ${styles.exploreButton}`} onClick={goToStartingComponent}>Explore an example</button>
         <button className={`${styles.button} ${styles.dataButton}`} onClick={() => { setModalOpen(true); }}>Use your own data</button>
         <input type="file" ref={fileInputRef} style={{ display: 'none' }} onChange={handleFileSelect} accept=".json" />
       </div>
