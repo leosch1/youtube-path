217a348e1dcb6fafe420ff227444acfe353e17bb
leosch1 <35605161+leosch1@users.noreply.github.com>
2024-04-14 12:52:51 +0200

    add iframe

--- example-data/exampleMostWatchedVideo.ts
+++ example-data/exampleMostWatchedVideo.ts
@@ -4,6 +4,6 @@ export const exampleMostWatchedVideo: Video = {
     "firstWatchedDate": new Date("2020-12-06T21:09:37.809Z"),
     "watchedCount": 23,
     "videoTitle": "Osman - Best Of",
-    "videoId": "https://www.youtube.com/watch?v=yi2xrCea8Ek",
+    "videoId": "yi2xrCea8Ek",
     "channelTitle": "Berner Jugendtreff"
 };
