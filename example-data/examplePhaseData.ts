67d313aaf092b2e64edd99aea57d9da1a1097d31
leosch1 <35605161+leosch1@users.noreply.github.com>
2024-04-17 23:27:37 +0200

    change phases

--- example-data/examplePhaseData.ts
+++ example-data/examplePhaseData.ts
@@ -4,7 +4,7 @@ export const examplePhaseData: PhaseData[] = [
   {
       "start": new Date("2021-05-14T08:17:03.131Z"),
       "end": new Date("2021-05-21T12:22:45.350Z"),
-      "title": "Ozzy Man Reviews",
+      "title": "PewDiePie",
       "count": 108,
       "density": 1.7432226035268793e-7,
       "normalizedDensity": 2.905371005878132e-12
@@ -12,7 +12,7 @@ export const examplePhaseData: PhaseData[] = [
   {
       "start": new Date("2021-08-07T09:35:05.107Z"),
       "end": new Date("2021-08-28T16:06:14.739Z"),
-      "title": "WILTY? Nope!",
+      "title": "GMHikaru",
       "count": 88,
       "density": 4.7881524602067093e-8,
       "normalizedDensity": 7.980254100344515e-13
@@ -20,7 +20,7 @@ export const examplePhaseData: PhaseData[] = [
   {
       "start": new Date("2022-07-19T21:37:55.762Z"),
       "end": new Date("2022-08-29T19:14:26.477Z"),
-      "title": "ジュキヤ/Jukiya",
+      "title": "WILTY? Nope!",
       "count": 88,
       "density": 2.4902436815644868e-8,
       "normalizedDensity": 4.1504061359408113e-13
