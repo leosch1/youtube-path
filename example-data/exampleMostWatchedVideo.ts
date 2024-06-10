866a35d227433dc33f5e3892ad8cab82bff5ec3d
leosch1 <35605161+leosch1@users.noreply.github.com>
2024-06-10 13:38:40 +0200

    change most watched video

--- example-data/exampleMostWatchedVideo.ts
+++ example-data/exampleMostWatchedVideo.ts
@@ -3,7 +3,7 @@ import { Video } from "../types/types";
 export const exampleMostWatchedVideo: Video = {
     "firstWatchedDate": new Date("2022-04-01T21:09:37.809Z"),
     "watchedCount": 23,
-    "videoTitle": "Osman - Best Of",
-    "videoId": "yi2xrCea8Ek",
-    "channelTitle": "Berner Jugendtreff"
+    "videoTitle": "ESSED SIE WIEDER GURKE?",
+    "videoId": "BpVR7AvP8LA",
+    "channelTitle": "Swissmeme TV"
 };
